9bbdac43a54719d13658be9da9ce99f5f860b318
Glitch (chatworkfilter) <none>
2025-02-10 16:11:53 +0000

    💾🌁 Checkpoint ./module/zalgo.js:72395350/568 ./module/mention.js:72395350/207 ./module/emoji.js:72395350/136

--- module/mention.js
+++ module/mention.js
@@ -1,12 +1,12 @@
 const block = require('../ctr/filter');
 
 async function to(body, roomId, accountId) {
-  if ((body.match(/[toall]/g) || []).length >= 10) {
+  if ((body.match(/toall/g) || []).length >= 10) {
     await block.blockMember(roomId, accountId);
     return "ok";
   }
   if ((body.match(/To:/g) || []).length >= 35) {
-      await block.blockMember(roomId, accountId);
+     await block.blockMember(roomId, accountId);
      return "ok";
   }
   return;
