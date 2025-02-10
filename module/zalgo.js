0383ff8c72528dd66b486a4c2ae88f40ff31f852
Glitch (chatworkfilter) <none>
2025-02-10 16:23:29 +0000

    🍨🍜 Checkpoint ./module/zalgo.js:72395350/2132

--- module/zalgo.js
+++ module/zalgo.js
@@ -3,12 +3,19 @@ const block = require('../ctr/filter');
 const zzalgo = /[\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]/;
 
 async function zalgo(body, roomId, accountId) {
-  const zalgoCount = (body.match(zzalgo) || []).length;
-  if (zalgoCount >= 1) {
-    console.log(zalgoCount);
+  let zalgoCount = 0;
+
+  for (let char of body) {
+    if (zzalgo.test(char)) {
+      zalgoCount++;
+    }
+  }
+  console.log("zalgo", zalgoCount)
+  if (zalgoCount >= 500) {
     await block.blockMember(roomId, accountId);
     return "ok";
   }
+  
   return;
 };
 
